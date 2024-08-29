44031d19e9548b8faaa6abc8897bc514d536a191
Samih Mustafa <samihmustafa@gmail.com>
2024-08-29 23:35:33 +0400

    phonebook 2.10 complete - refractor to components

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-
-const Person = ({ person }) => {
-  return <li>{person.name} {person.number}</li>
-}
-
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -66,27 +64,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={handleFilterChange}/>
-      </div>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName}  onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber}  onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <ul>
-      {personsToShow.map(person => 
-          <Person key={person.name} person={person} />
-        )}
-      </ul>
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,17 @@
+const Person = ({ person }) => {
+    return <li>{person.name} {person.number}</li>
+  }
+  
+
+const Persons = ({ personsToShow }) => {
+
+    return (
+        <ul>
+        {personsToShow.map(person => 
+            <Person key={person.name} person={person} />
+          )}
+        </ul>
+    )
+}
+
+export default Persons
