39856260105bad29c9f2bcec8d1e9072631bbf8f
Samih Mustafa <samihmustafa@gmail.com>
2024-08-29 23:21:48 +0400

    phonebook 2.8 complete - add number field to person

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 
 const Person = ({ person }) => {
-  return <li>{person.name}</li>
+  return <li>{person.name} {person.phoneNumber}</li>
 }
 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+        name: 'Arto Hellas', 
+        phoneNumber: '040-123456' 
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
 
   const handleNameChange = (event) => {
@@ -17,9 +21,23 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const addName = (event) => {
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
+
+    if(newName === '') {
+      alert('Name cannot be blank')
+      return
+    }
+    if(newNumber === '') {  
+      alert('Number cannot be blank')
+      return
+    }
 
     const foundPerson = persons.find(person => person.name === newName)
     console.log(foundPerson)
@@ -29,7 +47,8 @@ const App = () => {
     }
 
     const nameObject = {
-      name: newName
+      name: newName,
+      phoneNumber: newNumber
     }
   
     setPersons(persons.concat(nameObject))
@@ -39,9 +58,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName}  onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber}  onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
